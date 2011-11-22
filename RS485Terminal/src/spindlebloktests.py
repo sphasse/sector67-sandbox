2b4a84d367080fee1ca17394d4a10e06b0639cec
scott.hasse@gmail.com <scott.hasse@gmail.com@5124cfc0-7003-20d6-2c24-689dad8336d0>
2011-11-22 01:42:32 +0000

    implemented many of the spindleblok methods with unit testing via the mock command channel

--- RS485Terminal/src/spindlebloktests.py
+++ RS485Terminal/src/spindlebloktests.py
@@ -7,6 +7,7 @@ import unittest
 from spindleblokutils import DataUtils
 from spindleblokutils import MockCommandChannel
 from spindleblokutils import RS485Command
+from spindleblokutils import SpindleBlok
 #import re, time
 
 class Test(unittest.TestCase):
@@ -31,11 +32,11 @@ class Test(unittest.TestCase):
         self.assertEqual("00", DataUtils.apply_or_bit_mask("00", "00"))
 
     def test_signed_ints(self):
-        self.assertEqual(1, DataUtils.hex_string_to__signed_int("0001"))
-        self.assertEqual(-1, DataUtils.hex_string_to__signed_int("ffff"))
-        self.assertEqual(-32768, DataUtils.hex_string_to__signed_int("8000"))
-        self.assertEqual(-32767, DataUtils.hex_string_to__signed_int("8001"))
-        self.assertEqual(32767, DataUtils.hex_string_to__signed_int("7fff"))
+        self.assertEqual(1, DataUtils.hex_string_to_signed_int("0001"))
+        self.assertEqual(-1, DataUtils.hex_string_to_signed_int("ffff"))
+        self.assertEqual(-32768, DataUtils.hex_string_to_signed_int("8000"))
+        self.assertEqual(-32767, DataUtils.hex_string_to_signed_int("8001"))
+        self.assertEqual(32767, DataUtils.hex_string_to_signed_int("7fff"))
 
         self.assertEqual("0001", DataUtils.signed_int_to_hex_string(1, 16))
         self.assertEqual("ffff", DataUtils.signed_int_to_hex_string(-1, 16))
@@ -45,7 +46,7 @@ class Test(unittest.TestCase):
         # test the inverse functions actually invert
         for i in range(0,(2**16)):
             hexString = "%0.4x" % i
-            self.assertEqual(hexString, DataUtils.signed_int_to_hex_string(DataUtils.hex_string_to__signed_int(hexString), 16))
+            self.assertEqual(hexString, DataUtils.signed_int_to_hex_string(DataUtils.hex_string_to_signed_int(hexString), 16))
         
     def test_mock_channel(self):
         mock_channel = MockCommandChannel()
@@ -54,23 +55,45 @@ class Test(unittest.TestCase):
         request = RS485Command.create_read_request_command("1", "C00")
         mock_channel.send_command(request)
         reply = mock_channel.receive_reply("C00", 1)
-        self.assertEqual("%1C00ffffc", reply.full_string)
+        self.assertEqual("%1C00ffffQ", reply.full_string)
         self.assertEqual(RS485Command.READ_RESPONSE, reply.type)
 
         request = RS485Command.create_read_request_command("1", "C01")
         mock_channel.send_command(request)
         reply = mock_channel.receive_reply("C01", 1)
-        self.assertEqual("%1C01ffffd", reply.full_string)
+        self.assertEqual("%1C01ffffR", reply.full_string)
         self.assertEqual(RS485Command.READ_RESPONSE, reply.type)
 
         request = RS485Command.create_write_request_command("1", "C02", "0010")
         mock_channel.send_command(request)
         reply = mock_channel.receive_reply("C02", 1)
-        self.assertEqual("#1@", reply.full_string)
+        self.assertEqual("#1T", reply.full_string)
         self.assertEqual(RS485Command.WRITE_RESPONSE, reply.type)
         
         mock_channel.stop()
 
+    def test_vfd(self):
+        mock_channel = MockCommandChannel()
+        mock_channel.start()
+
+        vfd = SpindleBlok(mock_channel)
+        vfd.set_serial_control()
+        vfd.set_comm_timer_max(96)
+        vfd.set_rpm(0)
+        vfd.request_stop()
+
+        vfd.request_start()
+        vfd.set_rpm(1000)
+        self.assertEqual(1000, vfd.get_rpm())
+
+        vfd.set_rpm(0)
+        vfd.set_comm_timer_max(0)
+        vfd.request_stop()
+        vfd.set_local_control()
+
+
+        mock_channel.stop()
+        
     def test_user_interface(self):
         pass
 
