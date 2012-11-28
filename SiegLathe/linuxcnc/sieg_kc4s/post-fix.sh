e0f25da0a234f6bd35f31ced7f708fc5b56436b9
scott.hasse@gmail.com <scott.hasse@gmail.com@5124cfc0-7003-20d6-2c24-689dad8336d0>
2012-11-28 03:47:19 +0000

    temporary testing of pid

--- SiegLathe/linuxcnc/sieg_kc4s/post-fix.sh
+++ SiegLathe/linuxcnc/sieg_kc4s/post-fix.sh
@@ -1,3 +1,6 @@
 # comment the pwm input to insert the abs component
 sed -i -e 's/^\(net spindle-vel-cmd.*hm2_5i23.0.pwmgen.00.value$\)/#\1/' sieg_kc4s.hal
 sed -i -e 's/^loadrt abs names=abs.spindle.*/loadrt abs names=abs.spindle,abs.pwm/' sieg_kc4s.hal
+# for pid loop
+#sed -i -e 's/^\(net spindle-vel-cmd => abs.pwm.in\)/#\1/' sieg_kc4s.hal
+#sed -i -e 's/^loadrt scale names=scale.spindle.*/loadrt scale names=scale.spindle,scale.pid.s/' sieg_kc4s.hal
